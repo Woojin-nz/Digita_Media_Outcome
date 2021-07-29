28d7e71ea221a9d0115e89e667f4abd4964820c7
C204? <69278138+jeonw70450@users.noreply.github.com>
2021-07-29 12:30:38 +1200

    c

--- index.php
+++ index.php
@@ -4,19 +4,25 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
+    <meta name="description" content="Creative Commons Showcase">
+    <meta name="keywords" content="Creative Commons, images, gallery">
+    <meta name="author" content="Woojin Jeon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Website Title goes here</title>
+    <title>Creative Commons Showcase</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
     
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="layout.css"> 
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/simple-lightbox.css" />
     
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/layout.css"> 
+    <link rel="stylesheet" href="css/navigation.css" />
+
+    <!-- Link to JQuery -->
+    <script src="js/j_query_min.js"></script>
 </head>
     
 <body>
@@ -29,7 +35,29 @@
         </div>
 
         <div class="box nav">
-            Navigation goes here
+            <div class ="menu-header">
+
+            <i class="fa fa-bars"></i>
+
+            <div class="menu-content">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a class="dropbtn" href="#">Gallery <i class="fa fa-chevron-down">  
+                    </i></a>
+                        <ul class="dropdown">
+                        <li><a href='#'>Insects</a></li>
+                        <li><a href='#'>Space</a></li>
+                        <li><a href='#'>People</a></li>
+                    </ul>                
+
+                    </li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Links</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+
+            </div>
         </div>
 
         <div class = "box main">
