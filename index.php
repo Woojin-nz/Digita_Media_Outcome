2f4fee3f9c51190818426235af31c087a75c656e
C204? <69278138+jeonw70450@users.noreply.github.com>
2021-08-11 13:51:01 +1200

    cont

--- index.php
+++ index.php
@@ -29,40 +29,55 @@
     
     <div class="wrapper">
     
+    <div class="logo">
+        <a href="index.php"><img class="responsive" src="images/sphere_logo.png" alt="s" />
+        </a>
+        </div> <!-- / Logo-->
 
-        <div class="banner">
-            Banner goes here
-        </div>
+        <div class='top-title'>
+            <h2>CC Showcase</h2>
+            </div> <!-- / top title -->
 
-        <div class="box nav">
+     
+        <div class="header">
             <div class ="menu-header">
 
             <i class="fa fa-bars"></i>
 
             <div class="menu-content">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a class="dropbtn" href="#">Gallery <i class="fa fa-chevron-down">  
+                    <li><a class="nav" href="index.php">Home</a></li>
+                    <li><a class="nav dropbtn" href="#">Gallery <i class="fa fa-chevron-down">  
                     </i></a>
                         <ul class="dropdown">
-                        <li><a href='#'>Insects</a></li>
-                        <li><a href='#'>Space</a></li>
-                        <li><a href='#'>People</a></li>
+                        <li><a class="nav" href='index.php?page=insects'>Insects</a></li>
+                        <li><a class="nav" href='index.php?page=space'>Space</a></li>
+                        <li><a class="nav" href='index.php?page=people'>People</a></li>
                     </ul>                
 
                     </li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Links</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a class="nav" href="#">About</a></li>
+                    <li><a class="nav" href="index.php?page=links">Links</a></li>
+                    <li><a class="nav" href="#">Contact</a></li>
                 </ul>
             </div>
 
             </div>
         </div>
 
-        <div class = "box main">
-            Main content goes here
-        </div>
+        <?php 
+       
+        if(!isset($_REQUEST['page'])) {
+            include("home.php");
+        }
+
+        else {
+            $page=preg_replace('/[^0-9a-zA-Z]-/', '', $_REQUEST['page']);
+            include("$page.php");
+        }
+        
+        
+        ?>
        
 
         <div class="box footer">
---
+++ insects.php
@@ -0,0 +1,9 @@
+<div class = "box main">
+        <div class = "info-text">
+        <h2> Insect Gallery </h2>
+
+<p>
+    These insects images are a selection from Wikimedia commons featured pages on <a href="https://commons.wikimedia.org/wiki/Commons:Featured_pictures/Animals/Arthropods">Arthropods</a>
+</p>
+</div>
+</div>
